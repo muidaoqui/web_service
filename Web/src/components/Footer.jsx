4c8541ffda09a58adeb9c6ce738fb5b1326e9044
muidaoqui <muidao156@gmail.com>
2025-08-07 15:01:26 +0700

    domain-pg

--- Web/src/components/Footer.jsx
+++ Web/src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from '../assets/logo.png';
+
 function Footer() {
     return (
-        <div className="bg-white absolute bottom-0 border-t border-cyan-300 w-full">
+        <div className="bg-white border-t border-cyan-300 w-full">
             <div className="flex w-full justify-between items-center p-4 shadow-md divide-x divide-cyan-300">
                 <div className="w-1/4">
-                    <img src={logo} alt="Logo" className="w-auto " />
+                    <img src={logo} alt="Logo" className="w-auto" />
                 </div>
                 <div className="w-1/2 text-left flex flex-col gap-2 mx-8">
                     <p className="text-2xl font-bold text-cyan-400">CÔNG TY TNHH THƯƠNG MẠI VÀ DỊCH VỤ MD</p>
@@ -17,7 +18,7 @@ function Footer() {
                     <p>Instagram: instagram.com/md</p>
                 </div>
                 <div className="w-1/4 text-right">
-                    <p>Copyright © 2025 MD Company. </p>
+                    <p>Copyright © 2025 MD Company.</p>
                     <p>All rights reserved.</p>
                 </div>
             </div>
